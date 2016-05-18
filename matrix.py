82c71a4791d25f4b01ad619fa67a1453b5347e1c
Leif <leif.theden@gmail.com>
2016-05-18 17:09:02 -0500

    speed

--- matrix.py
+++ matrix.py
@@ -2,12 +2,11 @@
 """
 from itertools import chain
 from itertools import product
+from math import sqrt
 from os.path import join
-from random import randint, choice
+from random import random, randrange, choice
 
 import pygame
-
-from animation import Animation
 
 # Configuration
 charset = """abcdefghijklmnopqrstuvwxzy0123456789$+-*/=%"'#&_(),.;:?!\|{}<>[]^~"""
@@ -18,6 +17,9 @@ glyph_width = 14
 glyph_height = 16
 grid_spacing_x = 2
 grid_spacing_y = 2
+streamers = 0
+logo = None
+computed_values = list()
 
 
 class Glyph(pygame.sprite.Sprite):
@@ -25,31 +27,22 @@ class Glyph(pygame.sprite.Sprite):
     pos = 0, 0
     image = None
     char = None
-    index = 0
+    ttl = 0
+    char_index = 0
 
 
 class XYGroup(object):
     def __init__(self):
         self.layout = list()
-        self.q = None
+        self.sprites = list()
 
-    def __iter__(self):
-        for y, row in enumerate(self.layout):
-            for x, sprite in enumerate(row):
-                if sprite.value:
-                    yield x, y, sprite
-
-    def sprites(self):
-        return [i for i in chain.from_iterable(self.layout) if i.value]
+    @property
+    def active_sprites(self):
+        return (i for i in self.sprites if i.value)
 
     def draw(self, surface):
-        for sprite in self.sprites():
+        for sprite in self.active_sprites:
             surface.blit(sprite.image, sprite.pos)
-
-
-streamers = 0
-logo = None
-computed_values = list()
 
 
 def main():
@@ -57,28 +50,17 @@ def main():
     global screen_size
     max_streamers = 30
     grid = XYGroup()
-    cache = dict()
+    cache = list()
     frame_number = 0
     save_to_disk = 0
 
-    def render_glyph(font, glyph):
-        color = calc_color(glyph.value)
-        char = glyph.char
-        try:
-            return cache[(char, color)]
-
-        except KeyError:
-            image = font.render(char, 1, color)
-            image = pygame.transform.smoothscale(image, (glyph_width, glyph_height))
-            image.blit(scanline, (0, 0))
-            back = pygame.Surface(image.get_size())
-            back.blit(image, (0, 0))
-            cache[(char, color)] = back
-            return back
+    def get_glyph_image(glyph):
+        return cache[glyph.index][glyph.ttl]
 
     def calc_color(value):
-        value = int(round(value * 255., 0))
+        value *= 255.
         if value > 190:
+            value = int(round(value))
             value2 = int((value * 255) / 300)
             return value2, value, value2
         else:
@@ -89,24 +71,24 @@ def main():
     def new_glyph(font):
         glyph = Glyph()
         glyph.value = choice(computed_values)
-        glyph.char = choice(charset)
-        glyph.image = render_glyph(font, glyph)
+        glyph.index = randrange(len(charset))
+        glyph.image = get_glyph_image(glyph)
         return glyph
 
     def burn_glyph(glyph):
-        glyph.index = 0
+        glyph.ttl = 0
         glyph.value = 1
         x = glyph.pos[0] // (glyph_width + grid_spacing_x)
         y = glyph.pos[1] // (glyph_height + grid_spacing_y)
         burn_set.add((x, y, glyph))
 
     def update_grid():
-        for glyph in grid.sprites():
-            if not randint(0, 80):
-                glyph.char = choice(charset)
-            glyph.index += 1
-            glyph.value = computed_values[glyph.index]
-            glyph.image = render_glyph(font, glyph)
+        for glyph in grid.active_sprites:
+            if random() > .9:
+                glyph.index = randrange(len(charset))
+            glyph.ttl += 1
+            glyph.value = computed_values[glyph.ttl]
+            glyph.image = get_glyph_image(glyph)
 
         global streamers
         old_set = burn_set.copy()
@@ -123,7 +105,7 @@ def main():
 
         burn_set.difference_update(to_remove)
 
-        while not (streamers > max_streamers or randint(0, 2)):
+        while not (streamers > max_streamers or random() > .5):
             glyph = choice(grid.layout[0])
             burn_glyph(glyph)
             streamers += 1
@@ -149,6 +131,8 @@ def main():
             data[y][x] = glyph
             if glyph.value > .95:
                 burn_glyph(glyph)
+
+        grid.sprites = [i for i in chain.from_iterable(data)]
         grid.layout = data
 
     pygame.init()
@@ -158,17 +142,35 @@ def main():
     for y in range(0, glyph_height, 2):
         pygame.draw.line(scanline, (0, 0, 0, 128), (0, y), (glyph_width, y))
 
-    dummy = Glyph()
-    ani = Animation(dummy, value=0, initial=1, duration=5000, transition='out_circ')
-    group = pygame.sprite.Group()
-    group.add(ani)
-    while len(group):
-        ani.update(16)
-        computed_values.append(dummy.value)
+    time = 0.
+    duration = 5000.
+    while 1:
+        a = 1
+        b = 0
+        prog = min(1., time / duration)
+        p = prog - 1.0
+        t = sqrt(1.0 - p * p)
+        value = (a * (1. - t)) + (b * t)
+        computed_values.append(value)
+        time += 16
+        if prog >= 1:
+            break
 
-    burn_set = set()
     screen = init_screen(*screen_size)
     font = pygame.font.Font(font_name, font_size)
+    for index, char in enumerate(charset):
+        chars = list()
+        cache.append(chars)
+        for value in computed_values:
+            color = calc_color(value)
+            image = font.render(char, 1, color)
+            image = pygame.transform.smoothscale(image, (glyph_width, glyph_height))
+            image.blit(scanline, (0, 0))
+            back = pygame.Surface(image.get_size())
+            back.blit(image, (0, 0))
+            chars.append(back)
+
+    burn_set = set()
     init_group(*screen_size)
     clock = pygame.time.Clock()
 
@@ -183,6 +185,12 @@ def main():
         # 3: 80 fps
         # 4: 90 fps
 
+        # PC #2 trials
+        # 1: 48 fps (trial #4 equiv)
+        # 2: 60 fps
+        # 3: 74 fps
+        # 4: 90 fps
+
         for event in pygame.event.get():
             if event.type == pygame.VIDEORESIZE:
                 if not screen_size == (event.w, event.h):
@@ -194,7 +202,6 @@ def main():
                 running = False
 
         update_grid()
-        screen.fill((0, 0, 0))
         grid.draw(screen)
         screen.blit(logo, (0, 0), None, pygame.BLEND_RGBA_MULT)
 
