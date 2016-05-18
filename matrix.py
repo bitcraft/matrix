cc6b30f6fc127205779d375e61861a10102876c4
bitcraft <leif.theden@gmail.com>
2016-05-18 15:16:39 -0500

    speed

--- matrix.py
+++ matrix.py
@@ -1,23 +1,19 @@
 """ Matrix Screen Effect
 """
-from collections import deque
 from itertools import chain
 from itertools import product
 from os.path import join
-from random import randint, choice, random
-from threading import Event
-from operator import add
+from random import randint, choice
 
 import pygame
 
-from animation import Animation, Task
+from animation import Animation
 
 # Configuration
 charset = """abcdefghijklmnopqrstuvwxzy0123456789$+-*/=%"'#&_(),.;:?!\|{}<>[]^~"""
 font_name = join('resources', 'matrix code nfi.otf')
-font_size = 22
-screen_size = 1280, 1080
-screen_needs_redraw = Event()
+font_size = 32
+screen_size = 640, 480
 glyph_width = 14
 glyph_height = 16
 grid_spacing_x = 2
@@ -29,25 +25,7 @@ class Glyph(pygame.sprite.Sprite):
     pos = 0, 0
     image = None
     char = None
-
-
-class AnimationGroup(object):
-    def __init__(self):
-        self._animations = dict()
-
-    def add(self, ani):
-        for target, values in ani.targets:
-            self._animations[target] = ani
-
-    def remove(self, ani):
-        for target, values in ani.targets:
-            del self._animations[target]
-
-    def empty(self):
-        self._animations = dict()
-
-    def update(self, dt):
-        [ani.update(dt) for ani in self._animations.values()]
+    index = 0
 
 
 class XYGroup(object):
@@ -71,14 +49,13 @@ class XYGroup(object):
 
 streamers = 0
 logo = None
+computed_values = list()
 
 
 def main():
     # singletons, kinda
     global screen_size
     max_streamers = 30
-    animations = AnimationGroup()
-    tasks = pygame.sprite.Group()
     grid = XYGroup()
     cache = dict()
     frame_number = 0
@@ -94,8 +71,10 @@ def main():
             image = font.render(char, 1, color)
             image = pygame.transform.smoothscale(image, (glyph_width, glyph_height))
             image.blit(scanline, (0, 0))
-            cache[(char, color)] = image
-            return image
+            back = pygame.Surface(image.get_size())
+            back.blit(image, (0, 0))
+            cache[(char, color)] = back
+            return back
 
     def calc_color(value):
         value = int(round(value * 255., 0))
@@ -109,27 +88,27 @@ def main():
 
     def new_glyph(font):
         glyph = Glyph()
-        glyph.value = random()
+        glyph.value = choice(computed_values)
         glyph.char = choice(charset)
         glyph.image = render_glyph(font, glyph)
-        animations.add(Animation(glyph, value=0, duration=3000, transition='out_quad'))
         return glyph
 
     def burn_glyph(glyph):
-        animations.add(Animation(glyph, value=0, initial=1, duration=5000, transition='out_circ'))
+        glyph.index = 0
+        glyph.value = 1
         x = glyph.pos[0] // (glyph_width + grid_spacing_x)
         y = glyph.pos[1] // (glyph_height + grid_spacing_y)
         burn_set.add((x, y, glyph))
 
     def update_grid():
-        screen_needs_redraw.set()
         for glyph in grid.sprites():
             if not randint(0, 80):
                 glyph.char = choice(charset)
+            glyph.index += 1
+            glyph.value = computed_values[glyph.index]
             glyph.image = render_glyph(font, glyph)
 
         global streamers
-        screen_needs_redraw.set()
         old_set = burn_set.copy()
         to_remove = set()
         for token in old_set:
@@ -154,8 +133,6 @@ def main():
 
     def init_group(width, height):
         global logo
-        animations.empty()
-        tasks.add(Task(update_grid, 16, -1))
 
         cell_x = glyph_width + grid_spacing_x
         cell_y = glyph_height + grid_spacing_y
@@ -181,30 +158,30 @@ def main():
     for y in range(0, glyph_height, 2):
         pygame.draw.line(scanline, (0, 0, 0, 128), (0, y), (glyph_width, y))
 
+    dummy = Glyph()
+    ani = Animation(dummy, value=0, initial=1, duration=5000, transition='out_circ')
+    group = pygame.sprite.Group()
+    group.add(ani)
+    while len(group):
+        ani.update(16)
+        computed_values.append(dummy.value)
+
     burn_set = set()
     screen = init_screen(*screen_size)
     font = pygame.font.Font(font_name, font_size)
     init_group(*screen_size)
     clock = pygame.time.Clock()
-    fps = 60.
-    fps_log = deque(maxlen=10)
 
     running = True
     while running:
-        # somewhat smoother way to get fps and limit the framerate
-        clock.tick(fps * 2)
-        try:
-            fps_log.append(clock.get_fps())
-            fps = sum(fps_log) / len(fps_log)
-            td = 1 / fps * 1000
-        except ZeroDivisionError:
-            continue
+        clock.tick()
+        print(clock.get_fps())
 
-        if save_to_disk:
-            td = 16
-
-        animations.update(td)
-        tasks.update(td)
+        # trials:
+        # 1: 40 fps
+        # 2: 50 fps
+        # 3: 80 fps
+        # 4: 90 fps
 
         for event in pygame.event.get():
             if event.type == pygame.VIDEORESIZE:
@@ -212,11 +189,11 @@ def main():
                     screen_size = event.w, event.h
                 screen = init_screen(*screen_size)
                 init_group(*screen_size)
-                screen_needs_redraw.set()
 
             elif event.type == pygame.QUIT:
                 running = False
 
+        update_grid()
         screen.fill((0, 0, 0))
         grid.draw(screen)
         screen.blit(logo, (0, 0), None, pygame.BLEND_RGBA_MULT)
@@ -226,8 +203,8 @@ def main():
             frame_number += 1
             pygame.image.save(screen, filename)
 
-        screen_needs_redraw.clear()
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
